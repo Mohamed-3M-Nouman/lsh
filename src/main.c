31a8f3ac7a7fdaa954ec81fb0abaa051b05addd2
Mohamed Mohamed Nouman <anew3008@gmail.com>
2026-05-21 01:25:01 +0300

    Delivery of the second project

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
-/***************************************************************************//**
+/***************************************************************************/ /**
 
-  @file         main.c
+   @file         main.c
 
-  @author       Stephen Brennan
+   @author       Stephen Brennan
 
-  @date         Thursday,  8 January 2015
+   @date         Thursday,  8 January 2015
 
-  @brief        LSH (Libstephen SHell)
+   @brief        LSH (Libstephen SHell)
 
-*******************************************************************************/
+ *******************************************************************************/
 
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -17,29 +17,74 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_HISTORY 50
+char *history_list[MAX_HISTORY];
+int history_count = 0;
+
+void add_to_history(char *line)
+{
+  if (line == NULL || line[0] == '\0')
+  {
+    return;
+  }
+
+  if (history_count == MAX_HISTORY)
+  {
+    free(history_list[0]);
+    for (int i = 1; i < MAX_HISTORY; i++)
+    {
+      history_list[i - 1] = history_list[i];
+    }
+    history_count--;
+  }
+
+  history_list[history_count] = strdup(line);
+  history_count++;
+}
+
 /*
   Function Declarations for builtin shell commands:
  */
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
+int lsh_hi(char **args);
+int lsh_pwd(char **args);
+int lsh_echo(char **args);
+int lsh_history(char **args);
+int lsh_env(char **args);
+int lsh_export(char **args);
+int lsh_unset(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
  */
 char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
+    "cd",
+    "help",
+    "exit",
+    "hi",
+    "pwd",
+    "echo",
+    "history",
+    "env",
+    "export",
+    "unset"};
 
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
+int (*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit,
+    &lsh_hi,
+    &lsh_pwd,
+    &lsh_echo,
+    &lsh_history,
+    &lsh_env,
+    &lsh_export,
+    &lsh_unset};
 
-int lsh_num_builtins() {
+int lsh_num_builtins()
+{
   return sizeof(builtin_str) / sizeof(char *);
 }
 
@@ -48,19 +93,170 @@ int lsh_num_builtins() {
 */
 
 /**
+   @brief Builtin command: say hi .
+   @param args List of args.  args[0] is "hi".
+   @return Always returns 1, to continue executing.
+ */
+int lsh_hi(char **args)
+{
+  printf("Hi, I'm Mohamed Nouman, a AI Student at Assiut University.\n");
+  return 1;
+}
+
+/**
+   @brief Builtin command: print working directory.
+   @param args List of args.  Not evaluated.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_pwd(char **args)
+{
+  char cwd[1024];
+
+  if (getcwd(cwd, sizeof(cwd)) != NULL)
+  {
+    printf("🤖 [Mohamed Nouman's AI-Workspace]: \033[1;32m%s\033[0m\n", cwd);
+  }
+  else
+  {
+    perror("lsh: getcwd error");
+  }
+
+  return 1;
+}
+
+/**
    @brief Builtin command: change directory.
    @param args List of args.  args[0] is "cd".  args[1] is the directory.
    @return Always returns 1, to continue executing.
  */
 int lsh_cd(char **args)
 {
-  if (args[1] == NULL) {
+  if (args[1] == NULL)
+  {
     fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
+  }
+  else
+  {
+    if (chdir(args[1]) != 0)
+    {
       perror("lsh");
     }
   }
+  return 1;
+}
+
+/**
+   @brief Builtin command: echo arguments.
+   @param args List of args.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_echo(char **args)
+{
+  if (args[1] == NULL)
+  {
+    printf("✨ \033[1;35mNouman-Echo:\033[0m (Silence... nothing to echo!)\n");
+    return 1;
+  }
+
+  printf("🗣️ \033[1;36m");
+
+  int i = 1;
+
+  while (args[i] != NULL)
+  {
+    printf("%s ", args[i]);
+    i++;
+  }
+
+  printf("\033[0m\n");
+
+  return 1;
+}
+
+/**
+   @brief Builtin command: show history.
+   @param args List of args.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_history(char **args)
+{
+  printf("📜 \033[1;33mMohamed Nouman's Command History:\033[0m\n");
+
+  for (int i = 0; i < history_count; i++)
+  {
+    printf("  %d: %s\n", i + 1, history_list[i]);
+  }
+
+  return 1;
+}
+
+extern char **environ;
+
+int lsh_env(char **args)
+{
+  printf("🌍 \033[1;34mMohamed Nouman's Environment Variables:\033[0m\n");
+
+  int i = 0;
+  while (environ[i] != NULL)
+  {
+    printf("%s\n", environ[i]);
+    i++;
+  }
+
+  return 1;
+}
+
+int lsh_export(char **args)
+{
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: expected argument to \"export\"\n");
+    return 1;
+  }
+
+  char *name = strdup(args[1]);
+  char *value = strchr(name, '=');
+
+  if (value != NULL)
+  {
+    *value = '\0';
+    value++;
+
+    if (setenv(name, value, 1) != 0)
+    {
+      perror("lsh: export");
+    }
+    else
+    {
+      printf("✅ \033[1;32mExported successfully.\033[0m\n");
+    }
+  }
+  else
+  {
+    fprintf(stderr, "lsh: export format must be NAME=VALUE\n");
+  }
+
+  free(name);
+  return 1;
+}
+
+int lsh_unset(char **args)
+{
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: expected argument to \"unset\"\n");
+    return 1;
+  }
+
+  if (unsetenv(args[1]) != 0)
+  {
+    perror("lsh: unset");
+  }
+  else
+  {
+    printf("🗑️ \033[1;31mVariable '%s' unset successfully.\033[0m\n", args[1]);
+  }
+
   return 1;
 }
 
@@ -76,7 +272,8 @@ int lsh_help(char **args)
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
     printf("  %s\n", builtin_str[i]);
   }
 
@@ -105,18 +302,25 @@ int lsh_launch(char **args)
   int status;
 
   pid = fork();
-  if (pid == 0) {
+  if (pid == 0)
+  {
     // Child process
-    if (execvp(args[0], args) == -1) {
+    if (execvp(args[0], args) == -1)
+    {
       perror("lsh");
     }
     exit(EXIT_FAILURE);
-  } else if (pid < 0) {
+  }
+  else if (pid < 0)
+  {
     // Error forking
     perror("lsh");
-  } else {
+  }
+  else
+  {
     // Parent process
-    do {
+    do
+    {
       waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
@@ -133,13 +337,16 @@ int lsh_execute(char **args)
 {
   int i;
 
-  if (args[0] == NULL) {
+  if (args[0] == NULL)
+  {
     // An empty command was entered.
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0)
+    {
       return (*builtin_func[i])(args);
     }
   }
@@ -153,55 +360,46 @@ int lsh_execute(char **args)
  */
 char *lsh_read_line(void)
 {
-#ifdef LSH_USE_STD_GETLINE
-  char *line = NULL;
-  ssize_t bufsize = 0; // have getline allocate a buffer for us
-  if (getline(&line, &bufsize, stdin) == -1) {
-    if (feof(stdin)) {
-      exit(EXIT_SUCCESS);  // We received an EOF
-    } else  {
-      perror("lsh: getline\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-  return line;
-#else
-#define LSH_RL_BUFSIZE 1024
-  int bufsize = LSH_RL_BUFSIZE;
+  int bufsize = 1024;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
 
-  if (!buffer) {
+  if (!buffer)
+  {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  while (1) {
+  while (1)
+  {
     // Read a character
     c = getchar();
 
-    if (c == EOF) {
-      exit(EXIT_SUCCESS);
-    } else if (c == '\n') {
+    // If we hit EOF or newline, replace it with a null character and return.
+    if (c == EOF || c == '\n')
+    {
       buffer[position] = '\0';
       return buffer;
-    } else {
+    }
+    else
+    {
       buffer[position] = c;
     }
     position++;
 
     // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
+    if (position >= bufsize)
+    {
+      bufsize += 1024;
       buffer = realloc(buffer, bufsize);
-      if (!buffer) {
+      if (!buffer)
+      {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
   }
-#endif
 }
 
 #define LSH_TOK_BUFSIZE 64
@@ -214,25 +412,29 @@ char *lsh_read_line(void)
 char **lsh_split_line(char *line)
 {
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char*));
+  char **tokens = malloc(bufsize * sizeof(char *));
   char *token, **tokens_backup;
 
-  if (!tokens) {
+  if (!tokens)
+  {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
   token = strtok(line, LSH_TOK_DELIM);
-  while (token != NULL) {
+  while (token != NULL)
+  {
     tokens[position] = token;
     position++;
 
-    if (position >= bufsize) {
+    if (position >= bufsize)
+    {
       bufsize += LSH_TOK_BUFSIZE;
       tokens_backup = tokens;
-      tokens = realloc(tokens, bufsize * sizeof(char*));
-      if (!tokens) {
-		free(tokens_backup);
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens)
+      {
+        free(tokens_backup);
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
@@ -253,9 +455,13 @@ void lsh_loop(void)
   char **args;
   int status;
 
-  do {
-    printf("> ");
+  do
+  {
+    printf("Mohamed Nouman Shell> ");
     line = lsh_read_line();
+
+    add_to_history(line);
+
     args = lsh_split_line(line);
     status = lsh_execute(args);
 
@@ -281,4 +487,3 @@ int main(int argc, char **argv)
 
   return EXIT_SUCCESS;
 }
-
